489de7e53b589000eb52ebae14d9a000e3616ab5
Nhan Xuan Nhat <nhat.nhan@dena.jp>
2016-06-06 11:43:15 +0700

    add analytic level

---
+++ event/analytic.js
@@ -0,0 +1,17 @@
+// ====================================================================================================
+//
+// Cloud Code for analytic, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+require("share");
+var data = Spark.getData().data;
+var response= {};
+if(data.level_data){
+	var playerDataList = Spark.runtimeCollection("playerData");
+	for(var i = 1; i <= NUM_LEVEL ;i++){
+		response["level" + i] = playerDataList.count({"level":i});
+	}
+}
+Spark.setScriptData("data", response);
