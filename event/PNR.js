d6ed4642a7aede5b952f04817561235795b5719e
Minh Nguyen <minh.nguyen@dena.com>
2016-08-17 22:58:34 +0700

    Push notification for user when have new notice

--- event/PNR.js
+++ event/PNR.js
@@ -8,16 +8,18 @@ if (data.push_all_user) {
     var included_segments = ["All"];
     var excluded_segments = [];
     var message = data.message ? data.message : "Hey, can you back to play with us?";
-    var response = SendNewNotification(included_segments, excluded_segments, message).getResponseJson();
+    var title = data.title ? data.title : "Picachu Online";
+    var response = SendNewNotification(included_segments, excluded_segments, title, message).getResponseJson();
     Spark.setScriptData("response", response);
 }
 
-function SendNewNotification(included_segments, excluded_segments, message) {
+function SendNewNotification(included_segments, excluded_segments, title, message) {
   var jsonBody = {
     "app_id": "53aa05a0-16d7-4e30-894f-149c80736052",
     "included_segments": included_segments,
     "excluded_segments": excluded_segments,
-    "contents": {"en": "There is message push from GS to your application via OneSignal"},
+    "headings": {"en": title},
+    "contents": {"en": message}
   };
   var promise = Spark.getHttp("https://onesignal.com/api/v1/notifications").setHeaders({
     "Content-Type": "application/json;charset=utf-8",
